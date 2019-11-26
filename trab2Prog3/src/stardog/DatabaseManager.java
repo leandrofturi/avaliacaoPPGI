5978bfa23b1c7fa87b626f6a7058a17150c385df
Leandro Furlam <leandrofturi@gmail.com>
2019-11-26 14:52:28 -0300

    stardog

--- trab2Prog3/src/stardog/DatabaseManager.java
+++ trab2Prog3/src/stardog/DatabaseManager.java
@@ -2,4 +2,32 @@ package stardog;
 
 public class DatabaseManager {
 
+    Stardog aStardog = Stardog.builder().create();
+    try
+    {
+        try (AdminConnection aAdminConnection = AdminConnectionConfiguration.toEmbeddedServer()
+                .credentials("admin", "admin")
+                .connect()) {
+            if (aAdminConnection.list().contains("databaseTrab2")) {
+                aAdminConnection.drop("databaseTrab2");
+            }
+
+            aAdminConnection.disk("databaseTrab2").create();
+
+            try (Connection aConn = ConnectionConfiguration
+                    .to("databaseTrab2")
+                    .credentials("admin", "admin")
+                    .connect())
+
+        } finally {
+            // remove the database
+            if (aAdminConnection.list().contains("databaseTrab2")) {
+                aAdminConnection.drop("databaseTrab2");
+            }
+        }
+    }
+    finally
+    {
+        aStardog.shutdown();
+    }
 }
