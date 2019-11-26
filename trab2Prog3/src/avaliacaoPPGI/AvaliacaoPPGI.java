9b3b0371e4acfb77282520802bf69b61198744f3
Leandro Furlam <leandrofturi@gmail.com>
2019-11-26 14:33:43 -0300

    ant

---
+++ trab2Prog3/src/avaliacaoPPGI/AvaliacaoPPGI.java
@@ -0,0 +1,108 @@
+package avaliacaoPPGI;
+
+import exceptions.*;
+
+public class AvaliacaoPPGI {
+
+	public static void main(String[] args) {
+
+		//-d entradas/script-java/testes/01/in/docentes.csv -v entradas/script-java/testes/01/in/veiculos.csv -p entradas/script-java/testes/01/in/publicacoes.csv -q entradas/script-java/testes/01/in/qualis.csv -r entradas/script-java/testes/01/in/regras.csv -a 2017
+		
+		PPGI sistema = new PPGI();
+		
+		String flag = null;
+		String pathDocentes = null;
+		String pathVeiculos = null;
+		String pathPublicacoes = null;
+		String pathQualificacoes = null;
+		String pathRegras = null;
+		Integer anoRecredenciamento = null;
+		
+		for(int i = 0; i < args.length-1; i++) {
+			if(args[i].equals("--read-only")) {
+				if(flag == null)
+					flag = "R";
+				else
+					System.err.println(new Desconhecido().getMessage());
+			}
+			else if(args[i].equals("--write-only")) {
+				if(flag == null)
+					flag = "W";
+				else
+					System.err.println(new Desconhecido().getMessage());
+			}
+			else if(args[i].equals("-d")) {
+				pathDocentes = args[i+1];
+			}
+			else if(args[i].equals("-v")) {
+				pathVeiculos = args[i+1];
+			}
+			else if(args[i].equals("-p")) {
+				pathPublicacoes = args[i+1];
+			}
+			else if(args[i].equals("-q")) {
+				pathQualificacoes = args[i+1];
+			}
+			else if(args[i].equals("-r")) {
+				pathRegras = args[i+1];
+			}
+			else if(args[i].equals("-a")) {
+				try {
+					anoRecredenciamento = Integer.parseInt(args[i+1].trim());
+				} catch (NumberFormatException e) {
+					System.err.println(new Desconhecido().getMessage());
+				}
+			}
+		}
+
+		if(flag == null) {
+			try {
+				sistema.carregaArquivoDocentes(pathDocentes);
+				sistema.carregaArquivoVeiculos(pathVeiculos);
+				sistema.carregaArquivoQualificacoes(pathQualificacoes);
+				sistema.carregaArquivoPublicacoes(pathPublicacoes);
+				sistema.carregaArquivoPontuacoes(pathRegras);
+				
+				sistema.escreveArquivoRecredenciamento(anoRecredenciamento);
+				sistema.escreveArquivoPublicacoes();
+				sistema.escreveArquivoEstatisticas();
+			}
+			catch (ErroDeIO | ErroDeFormatacao | CodigoRepetido | VeiculoDesconhecido | SiglaVeiculoNaoDefinida | QualiDesconhecidoVeiculo | Desconhecido | CodNaoDefinido | QualiDesconhecidoRegra e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		
+		else if(flag.equals("R")) {
+			try {
+				sistema.carregaArquivoDocentes(pathDocentes);
+				sistema.carregaArquivoVeiculos(pathVeiculos);
+				sistema.carregaArquivoQualificacoes(pathQualificacoes);
+				sistema.carregaArquivoPublicacoes(pathPublicacoes);
+				sistema.carregaArquivoPontuacoes(pathRegras);
+				
+				PPGI.serializar(sistema);
+			}
+			catch (ErroDeIO | ErroDeFormatacao | CodigoRepetido | VeiculoDesconhecido | Desconhecido | CodNaoDefinido | SiglaVeiculoNaoDefinida | QualiDesconhecidoVeiculo | QualiDesconhecidoRegra e) {
+				System.err.println(e.getMessage());
+			}
+
+		}
+		else if(flag.equals("W")) {
+			try {
+				sistema = PPGI.desserializar();
+				
+				sistema.escreveArquivoRecredenciamento(anoRecredenciamento);
+				sistema.escreveArquivoPublicacoes();
+				sistema.escreveArquivoEstatisticas();
+			}
+			catch (ErroDeIO | Desconhecido e) {
+				System.err.println(e.getMessage());
+			}
+
+		}
+		else
+			System.err.println(new Desconhecido().getMessage());
+
+	}
+
+}
---
+++ trab2Prog3/src/serialize/Serialize.java
@@ -0,0 +1,77 @@
+package serialize;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import exceptions.Desconhecido;
+import exceptions.ErroDeIO;
+
+public class Serialize {
+
+	public static void serializar(Object obj) throws ErroDeIO, Desconhecido {
+		
+		/*
+		File dirData = new File("data");
+		if(!dirData.exists()) {
+			try {
+				dirData.mkdir();
+		    } catch(SecurityException se) {
+		    	se.getMessage();
+		    	throw new Desconhecido();
+		    }
+		}
+		*/
+		
+		try {
+			/*
+			File dir = new File("data/tmp");
+			if(!dir.exists()) {
+				try {
+					dir.mkdir();
+			    } catch(SecurityException se) {
+			    	se.getMessage();
+			    	throw new Desconhecido();
+			    }
+			}
+			*/
+			
+			//File file = new File("data/tmp/recredenciamento.dat");
+			File file = new File("recredenciamento.dat");
+			FileOutputStream fileOut = new FileOutputStream(file);
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			
+			out.writeObject(obj);
+
+			out.close();
+			fileOut.close();
+			//System.out.printf("Serialização dos dados foi salvo em data/tmp/recredenciamento.dat" + '\n');
+	    } catch(IOException i) {
+	    	throw new ErroDeIO();
+	    }
+	}
+	
+	public static Object desserializar() throws ErroDeIO, Desconhecido {
+		
+	    try {
+	    	//FileInputStream fileIn = new FileInputStream("data/tmp/recredenciamento.dat");
+	    	FileInputStream fileIn = new FileInputStream("recredenciamento.dat");
+	    	ObjectInputStream in = new ObjectInputStream(fileIn);
+	       
+	    	Object obj = in.readObject();
+	        
+	    	in.close();
+	    	fileIn.close();
+	    	//System.out.println("Dados desserializados" + '\n');
+	    	return obj;
+	    } catch(IOException i) {
+	    	throw new ErroDeIO();
+	    } catch(ClassNotFoundException c) {
+	    	throw new Desconhecido();
+	    }
+	}
+	
+}
